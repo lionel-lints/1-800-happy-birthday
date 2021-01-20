ccbd995f5512b7a86078d4c6b2c1348d6f99065c
Lionel Lints <lionel.lints@gmail.com>
2021-01-20 08:44:26 -0800

    👒 small edit to AudioPlayer

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -6,10 +6,9 @@ class AudioPlayer extends Component {
   constructor(props) {
     super(props);
 
-    // The source audio files that we'll switch between
-    this.sources = props.sources;
-
     this.state = {
+      // The source audio files that we'll switch between
+      sources: props.sources,
       currentSrcIndex: 0,
       playing: false
     };
@@ -20,7 +19,7 @@ class AudioPlayer extends Component {
 
   handleNext() {
     const nextIndex =
-      this.state.currentSrcIndex >= this.sources.length - 1
+      this.state.currentSrcIndex >= this.state.sources.length - 1
         ? 0
         : this.state.currentSrcIndex + 1;
     this.setState({ currentSrcIndex: nextIndex });
@@ -39,15 +38,15 @@ class AudioPlayer extends Component {
           // src prop into ReactHowler which will destroy our
           // currently playing Howler.js and initialize
           // a new Howler.js instance
-          src={this.sources[this.state.currentSrcIndex]}
+          src={this.state.sources[this.state.currentSrcIndex]}
         />
+        <div>
+          Current Recording: {this.state.currentSrcIndex + 1} of{" "}
+          {this.state.sources.length}
+        </div>
         <button className="full" onClick={this.handleNext}>
           Next Recording
         </button>
-        <div>
-          Current Recording: {this.state.currentSrcIndex + 1} of{" "}
-          {this.sources.length}
-        </div>
         <button onClick={this.togglePlay}>
           {this.state.playing ? "Pause" : "Play"}
         </button>
