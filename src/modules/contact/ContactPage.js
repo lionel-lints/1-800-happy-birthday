288c2f3214220bd6c203e5ecc61166472ab172ce
Lionel Lints <lionel.lints@gmail.com>
2021-03-02 07:33:07 -0800

    add css and context language stuff

--- src/modules/contact/ContactPage.js
+++ src/modules/contact/ContactPage.js
@@ -2,7 +2,32 @@ import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
 
-import { NavBar, Hero } from "@/modules/_common";
+import {
+  NavBar,
+  Header,
+  Hero,
+  LanguageContextConsumer
+} from "@/modules/_common";
+
+import { contact } from "@/assets/locales/data.json";
+
+const StyledAdvisorSection = styled.section`
+  margin-left: 15%;
+  width: 70vw;
+  height: 10vh;
+  margin-top: 5vh;
+  margin-bottom: 5vh;
+  h3 {
+    font-family: Pinyon Script;
+    font-size: 60px;
+    text-align: center;
+  }
+  p {
+    font-family: "Roboto Mono", monospace;
+    font-size: 20px;
+    text-align: center;
+  }
+`;
 
 const StyledContactPage = styled.div`
   background-color: black;
@@ -12,6 +37,12 @@ const StyledContactPage = styled.div`
 `;
 
 const StyledContactDetails = styled.section`
+  margin-left: 15%;
+  width: 70vw;
+  height: 10vh;
+  margin-top: 5vh;
+  margin-bottom: 5vh;
+
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -21,30 +52,42 @@ const StyledContactDetails = styled.section`
   }
 `;
 
+const StyledAnchor = styled.a`
+  font-family: BradleyDisplay;
+  font-size: 40px;
+  margin-bottom: 5vh;
+`;
+const StyledParagraph = styled.p`
+  font-family: Pinyon Script;
+  font-size: 40px;
+  text-align: center;
+`;
+
+const StyledBlurb = styled(StyledParagraph)`
+  margin-bottom: 5vh;
+`;
+
 const ContactPage = () => (
-  <StyledContactPage>
-    <NavBar />
-    <Hero />
-    <StyledContactDetails>
-      <h1>Contact Us</h1>
-      <p>
-        Would you like your loved one to be included in this project or want to
-        work together? Please get in touch via the following methods.
-      </p>
-      <p>Email Us</p>
-      <a href="1800happybirthday@gmail.com">1800happybirthday@gmail.com</a>
-      <p>Send us a message on Instagram</p>
-      <a href="https://www.instagram.com/1800HappyBirthday">
-        @1800HappyBirthday
-      </a>
-      <p>
-        1800 Happy Birthday is a voicemail project created to honor the Black
-        and Brown victims of Police Killings and Systemic Racism by allowing
-        people to leave and listen to voicemails left for the victims on their
-        birthday
-      </p>
-    </StyledContactDetails>
-  </StyledContactPage>
+  <LanguageContextConsumer>
+    {context => (
+      <StyledContactPage>
+        <NavBar />
+        <Hero />
+        <StyledContactDetails>
+          <Header title={contact.title[context.lang]} />
+          <StyledBlurb>{contact.text[context.lang]}</StyledBlurb>
+          <StyledParagraph>{contact.email[context.lang]}</StyledParagraph>
+          <StyledAnchor href="1800happybirthday@gmail.com">
+            1800happybirthday@gmail.com
+          </StyledAnchor>
+          <StyledParagraph>{contact.instagram[context.lang]}</StyledParagraph>
+          <StyledAnchor href="https://www.instagram.com/1800HappyBirthday">
+            @1800HappyBirthday
+          </StyledAnchor>
+        </StyledContactDetails>
+      </StyledContactPage>
+    )}
+  </LanguageContextConsumer>
 );
 
 ContactPage.propTypes = {};
