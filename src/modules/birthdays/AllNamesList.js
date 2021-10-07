ca960735d86dc40727fb257c303f356c19b05ad7
natatat <natuduwela@gmail.com>
2021-10-07 15:11:06 -0700

    AllNames, ActiveNames styled components for the HomePage

---
+++ src/modules/birthdays/AllNamesList.js
@@ -0,0 +1,63 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const StyledList = styled.ul`
+  padding: 0;
+  margin: 0;
+  text-align: center;
+  margin: 2%;
+  opacity: 0.1;
+  position: fixed;
+  top: 0;
+  transition: all 1s ease-out;
+  user-select: none;
+  padding-bottom: 10rem;
+  z-index: -1;
+`;
+
+const StyledName = styled.li`
+  display: inline;
+  list-style-type: circle;
+  margin-right: 0.5rem;
+
+  font-family: BradleyMicro;
+  color: ${p => (p.isEven ? "gray" : "lightgray")};
+  font-size: 2rem;
+  line-height: 3rem;
+`;
+
+const AllNamesList = ({ data }) => {
+  return (
+    <StyledList>
+      {data.map((row, index) => {
+        const name = row.fields.find(field => field.name === "Name").value;
+
+        return (
+          <StyledName key={name} isEven={index % 2 === 0}>
+            {name}
+          </StyledName>
+        );
+      })}
+    </StyledList>
+  );
+};
+
+AllNamesList.propTypes = {
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      fields: PropTypes.arrayOf(
+        PropTypes.shape({
+          name: PropTypes.string
+        })
+      ),
+      name: PropTypes.string
+    })
+  )
+};
+
+AllNamesList.defaultProps = {
+  data: []
+};
+
+export default AllNamesList;
--- src/modules/birthdays/HomePage.js
+++ src/modules/birthdays/HomePage.js
@@ -1,33 +1,100 @@
-import React, { useLayoutEffect } from "react";
+import React, { useState, useEffect, useLayoutEffect } from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import _ from "underscore";
 
-import { NavBar, Footer } from "@/modules/_common";
-import AllNames from "@/modules/birthdays/AllNames.js";
+import Airtable from "airtable";
+import tableHasPublishedColumn from "@/utils/tableHasPublishedColumn";
 
-const StyledHomePage = styled.div``;
+import { NavBar, Footer, Blurb, Marquee } from "@/modules/_common";
+import ActiveNames from "@/modules/birthdays/ActiveNames.js";
+import AllNamesList from "@/modules/birthdays/AllNamesList.js";
+
+const StyledHomePage = styled.div`
+  color: white;
+  position: relative;
+`;
 
 const HomePage = () => {
-  useLayoutEffect(() => {
-    // scroll to top of page on link transition
-    window.scrollTo(0, 0);
+  const [data, setData] = useState(null);
+
+  // useLayoutEffect(() => {
+  //   // scroll to top of page on link transition
+  //   window.scrollTo(0, 0);
+  // });
+
+  useEffect(() => {
+    const allRows = [];
+
+    const base = new Airtable({
+      apiKey: process.env.AIRTABLE_API_KEY
+    }).base(process.env.BASE_ID);
+
+    tableHasPublishedColumn(base, includePublished =>
+      base(process.env.TABLE_ID)
+        .select({
+          view: process.env.VIEW,
+          ...(includePublished ? { filterByFormula: "{Published}" } : {})
+        })
+        .eachPage(
+          function page(records, fetchNextPage) {
+            records.forEach(row => {
+              const fieldsArray = _.map(row.fields, (value, name) => ({
+                name,
+                value
+              }));
+
+              const fieldOrderMapped = process.env.FIELD_ORDER
+                ? _.object(
+                    process.env.FIELD_ORDER.split(",").map((field, idx) => [
+                      field,
+                      idx
+                    ])
+                  )
+                : null;
+              const fields = fieldOrderMapped
+                ? _.sortBy(fieldsArray, field => fieldOrderMapped[field.name])
+                : fieldsArray;
+
+              allRows.push({
+                ...row,
+                fields
+              });
+            });
+
+            // calls page function again while there are still pages left
+            fetchNextPage();
+          },
+          err => {
+            if (err) {
+              // eslint-disable-next-line no-console
+              console.error(err);
+            }
+
+            setData(allRows);
+          }
+        )
+    );
   });
 
   return (
     <StyledHomePage>
       <NavBar />
-      <AllNames />
+      <Blurb />
+      {data ? (
+        <>
+          <ActiveNames data={data} />
+          <AllNamesList data={data} />
+        </>
+      ) : null}
+      <Marquee />
       <Footer />
     </StyledHomePage>
   );
 };
 
-HomePage.propTypes = {
-  currentPage: PropTypes.number
-};
+HomePage.propTypes = {};
 
-HomePage.defaultProps = {
-  currentPage: 1
-};
+HomePage.defaultProps = {};
 
 export default HomePage;
