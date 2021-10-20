d01d75551c051fa216d76c5444d25f269c48cd62
natatat <natuduwela@gmail.com>
2021-10-20 14:56:05 -0700

    simplify the way we get data from Airtable
    + fix async error by installing regenerator-runtime

--- src/modules/birthdays/HomePage.js
+++ src/modules/birthdays/HomePage.js
@@ -1,14 +1,11 @@
 import React, { useState, useEffect, useLayoutEffect } from "react";
-import PropTypes from "prop-types";
 import styled from "styled-components";
-import _ from "underscore";
-
-import Airtable from "airtable";
-import tableHasPublishedColumn from "@/utils/tableHasPublishedColumn";
 
 import { PageHeader, Footer, Blurb, Marquee } from "@/modules/_common";
 import ActiveNames from "@/modules/birthdays/ActiveNames.js";
 import AllNamesList from "@/modules/birthdays/AllNamesList.js";
+
+import AirtableClient from "@/lib/AirtableClient";
 
 const StyledHomePage = styled.div`
   color: white;
@@ -24,58 +21,14 @@ const HomePage = () => {
   // });
 
   useEffect(() => {
-    const allRows = [];
+    const getData = async () => {
+      const response = await AirtableClient.fetchData();
+      console.log(response);
+      setData(response);
+    };
 
-    const base = new Airtable({
-      apiKey: process.env.AIRTABLE_API_KEY
-    }).base(process.env.BASE_ID);
-
-    tableHasPublishedColumn(base, includePublished =>
-      base(process.env.TABLE_ID)
-        .select({
-          view: process.env.VIEW,
-          ...(includePublished ? { filterByFormula: "{Published}" } : {})
-        })
-        .eachPage(
-          function page(records, fetchNextPage) {
-            records.forEach(row => {
-              const fieldsArray = _.map(row.fields, (value, name) => ({
-                name,
-                value
-              }));
-
-              const fieldOrderMapped = process.env.FIELD_ORDER
-                ? _.object(
-                    process.env.FIELD_ORDER.split(",").map((field, idx) => [
-                      field,
-                      idx
-                    ])
-                  )
-                : null;
-              const fields = fieldOrderMapped
-                ? _.sortBy(fieldsArray, field => fieldOrderMapped[field.name])
-                : fieldsArray;
-
-              allRows.push({
-                ...row,
-                fields
-              });
-            });
-
-            // calls page function again while there are still pages left
-            fetchNextPage();
-          },
-          err => {
-            if (err) {
-              // eslint-disable-next-line no-console
-              console.error(err);
-            }
-
-            setData(allRows);
-          }
-        )
-    );
-  });
+    getData();
+  }, []);
 
   return (
     <StyledHomePage>
