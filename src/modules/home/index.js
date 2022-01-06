082662f18aef3cbd05ad8fefeaccae30f8aa0fb3
natatat <natuduwela@gmail.com>
2022-01-06 12:11:18 -0800

    fix rendering error

--- src/modules/home/index.js
+++ src/modules/home/index.js
@@ -3,13 +3,5 @@ import AllNamesList from "@/modules/home/AllNamesList";
 import Loader from "@/modules/home/Loader";
 import LoaderBlurb from "@/modules/home/LoaderBlurb";
 import HomePage from "@/modules/home/HomePage";
-import VoicemailPlayer from "@/modules/home/VoicemailPlayer";
 
-export {
-  ActiveNames,
-  AllNamesList,
-  Loader,
-  LoaderBlurb,
-  HomePage,
-  VoicemailPlayer
-};
+export { ActiveNames, AllNamesList, Loader, LoaderBlurb, HomePage };
